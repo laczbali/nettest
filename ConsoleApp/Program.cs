9cbe99d1b9a56dbd7fad018b31e77ab9ccb0249c
LaczkóBalázs <lab@frankly.dk>
2023-12-24 15:26:04 +0100

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp;
+
+new Tester(
+    TimeSpan.FromSeconds(1),
+    new Uri("https://google.com"),
+    Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+        "nettest.log"))
+.Start();
---
+++ ConsoleApp/Tester.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.NetworkInformation;
+
+namespace ConsoleApp;
+public class Tester
+{
+    private readonly TimeSpan testInterval;
+    private readonly Uri targetSite;
+    private readonly string logFilePath;
+
+    private Ping pinger = new Ping();
+    private int lastConsoleLineLength = 0;
+
+    public Tester(TimeSpan testInterval, Uri targetSite, string logFilePath)
+    {
+        this.testInterval = testInterval;
+        this.targetSite = targetSite;
+        this.logFilePath = logFilePath;
+
+        if (!File.Exists(logFilePath))
+        {
+            File.Create(logFilePath).Close();
+        }
+
+        if (!PingTarget())
+        {
+            throw new ArgumentException("Target site is not reachable");
+        }
+    }
+
+    public void Start()
+    {
+        while (true)
+        {
+            var pingResult = PingTarget() ? "OK" : "FAIL";
+            var currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            LogToConsole($"{currentTime} - {pingResult}");
+            File.AppendAllText(logFilePath, $"{currentTime};{pingResult}" + Environment.NewLine);
+            Thread.Sleep(testInterval);
+        }
+    }
+
+    private bool PingTarget()
+    {
+        try
+        {
+            var reply = pinger.Send(targetSite.Host);
+            return reply.Status == IPStatus.Success;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    private void LogToConsole(string message)
+    {
+        if (message.Length < lastConsoleLineLength)
+        {
+            message += new string(' ', lastConsoleLineLength - message.Length);
+        }
+
+        Console.Write(message);
+        Console.SetCursorPosition(0, Console.CursorTop);
+        lastConsoleLineLength = message.Length;
+    }
+}
